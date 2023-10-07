3928a0743e21365b2ca053ee6bff75051aa3b864
zillaspani <garofalo.francesco@outlook.it>
2023-10-07 06:51:01 +0200

    Restore classe Simone

--- Versione1/Attuatore.py
+++ Versione1/Attuatore.py
@@ -1,10 +1,10 @@
-import logging
-import asyncio
-import aiocoap
+import sys
+
 from colorama import Fore
+from abc import abstractmethod
 
 
-from aiocoap import *
+
 class Attuatore:
     def __init__(self, server_uri):
         self.server_uri = server_uri
@@ -13,54 +13,59 @@ class Attuatore:
     def get_stato(self):
         return self.stato
 
-    async def invia_richiesta(self):
-        protocol = await aiocoap.Context.create_client_context()
-        request = aiocoap.Message(code=aiocoap.GET, uri=self.server_uri)
-        try:
-            response = await protocol.request(request).response
-        except aiocoap.error.RequestTimedOut:
-            print(Fore.GREEN+"Richiesta al server CoAP scaduta")
-            return None
-        if response.code.is_successful():
-            try:
-                numero_intero = int(response.payload)
-                return numero_intero
-            except ValueError:
-                print(Fore.GREEN+"Il server ha inviato una risposta non valida")
-                return None
-        else:
-            print(Fore.GREEN+f"Errore nella risposta del server: {response.code}")
-            return None
-    async def esegui(self, ):
+    
+    
+    #Metodo comune a tutti gli attuatori per stampare informazioni di debug 
+    def print_info(self, current_uri, network_interfaces):
+        #current_uri = os.path.abspath(__file__)
+        print(Fore.GREEN+ "URI dell'attuatore corrente:", current_uri)
+        print()
+        #network_interfaces = psutil.net_if_addrs()
+        interface_name = "eth0"
+        ip_address = network_interfaces[interface_name][0].address
+        print(f"Indirizzo IP dell'interfaccia {interface_name} dell'attuatore: {ip_address}")
+        print()
+    
+    #Metodo astratto per implementare secondo quali politiche/librerie inviare i dati al server
+    @abstractmethod
+    def invia_richiesta(self):
+        pass
+        
+    #Metodo comune a tutti gli attuatori che analizzano il numero ricevuto dal server
+    async def esegui(self):
         comando = await self.invia_richiesta()
         if comando is not None:
             print(Fore.GREEN+f"Risposta dal server: {comando}")
             if comando == 1 :
-                print(Fore.GREEN+f"Bisogna accendere l'attuatore")
-                '''questo comporterà la chiamata di un metodo nella classe campo che si occuperà di 
-                    decrementare la temperatura e incrementare l'umidità nel tempo'''
-                self.stato= True
-                print(self.stato)
-            elif comando == 0: 
-                print(Fore.GREEN+f"Bisogna spengnere l'attuatore")
-                '''questo comporterà la chiamata di un metodo nella classe campo che si occuperà di 
-                    incrementare la temperatura e decrementare l'umidità nel tempo'''
-                self.stato=False
-            else:
-                print(Fore.GREEN+f"I valori sono buoni, lascia l'attuatore nel suo stato attuale") 
-                #doNothing nella classe campo
+                if self.stato:
+                    print("L'attuatore è già acceso, è necessario mantenere questo stato")
+                else:
+                    print(Fore.GREEN+f"Bisogna accendere l'attuatore")
+                    self.stato= True
+                    print(Fore.GREEN+f"L'attuatore è stato acceso correttamente")
+            elif comando == 0:
+                if not self.stato: 
+                    print("L'attuatore è già spento, è necessario mantenere questo stato")
+                else:
+                    print(Fore.GREEN+f"Bisogna spengnere l'attuatore")
+                    self.stato=False
+                    print (Fore.GREEN+ f"L'attuatore è stato spento correttamente")
                 
-            
-
-
+            else:
+                if self.stato==False:
+                    stato="spento"
+                else:
+                    stato="acceso"
+                print(Fore.GREEN+f"I valori sono buoni, l'attuatore resta {stato}")
+            print()
+                
         else:
             print(Fore.GREEN+"Impossibile ottenere una risposta dal server")
-
-'''
-if __name__ == "__main__":
-    server_uri = "coap://localhost/data"  # Cambia con l'URI del tuo server CoAP
-    client = Attuatore(server_uri)
     
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(client.esegui())
-'''
+    
+
+#Controllo fatto quando si lancia l'attuatore per verificare che sia specificato il server
+if len(sys.argv) != 2:
+    print("Usage: python3 Attuatore*.py <server_uri>")
+    sys.exit(1)
+ 
