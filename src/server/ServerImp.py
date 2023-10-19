5b04a43a7fad2f476ed2babdc05670c63ef8c156
zillaspani <garofalo.francesco@outlook.it>
2023-10-19 19:20:36 +0200

    Server & logging works, log messages traduced.

--- src/server/ServerImp.py
+++ src/server/ServerImp.py
@@ -9,25 +9,30 @@ class server(AbstractServer):
     def sendResponse(self,response):
         return response
 
-    
+def __init__(self):
+        logging.basicConfig(level=logging.INFO)
+        ip_address = socket.gethostbyname(socket.gethostname())
+        logging.info(f"Running server aiocoap on %s ",ip_address)
+
+
 async def main():
     try:
         s=server()
         root = aiocoap.resource.Site()
         root.add_resource(('data',), s.DataResource())
         root.add_resource(('receive',), s.ReceiveState())
-        ip_address = socket.gethostbyname(socket.gethostname())
-        logging.info(f"Avvio server aiocoap su %s ",ip_address)
+        root.add_resource(('dummy',), s.DummyResource())
+        logging.info(f"Resource tree OK")
         await aiocoap.Context.create_server_context(root)
         await asyncio.get_running_loop().create_future()
         #asyncio.get_event_loop().run_until_complete(aiocoap.Context.create_server_context(root))
         #ip_address = socket.gethostbyname(socket.gethostname())
         #logging.info(f"Avvio server aiocoap su %s ",ip_address)
-        #asyncio.get_event_loop().run_forever()       
+        #asyncio.get_event_loop().run_forever()     
     
     except Exception as ex:
         logging.error(ex)
-        logging.error("server non instanziato")
+        logging.error("Server cannot be instantiated")
         exit()
 
 if __name__ == "__main__":
