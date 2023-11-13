aafbdb8e53fb9ff51e1bad862b82ee85d03b8c14
Sodus4 <sodusss12@gmail.com>
2023-11-13 16:19:52 +0100

    attuatore e sensore ok

--- src/attuatore/AttuatoreSecure.py
+++ src/attuatore/AttuatoreSecure.py
@@ -12,12 +12,16 @@ from Attuatore import Attuatore
 from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Util.Padding import pad, unpad
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 
 class AttuatoreSecure(Attuatore):
     '''chiavi inizialmente messe qui per prova, 16 byte per AES, 32 per HMAC, da spostare il un file di configurazione poi'''
-    aes_key=b'u\xa3\\\x96\x08\x8e\xd3jc\x0f\xdbq3\xc4\x1d\xde'
-    hmac_key=b'\xcbV\xfdU\xf5\xcf\xcb\xcczI\xa6p\xaf\xe8=\x18\x18\x17`\xe4\xb0\xaf$a\x0c/\xc8\xach\xda\x92\xc2'
+    aes_key=b''
+    hmac_key=b''
+    id_client=0
 
     def check_payload(self, ct, tag, key): 
         check=self.tag_hmac_sha256(ct, key)
@@ -56,7 +60,70 @@ class AttuatoreSecure(Attuatore):
             tag=b64encode(h.digest()).decode('utf-8')
             #ritorna un tag a stringa
             return tag 
+        
+    def private_client_key_decrypt(self, response_string, path_private_key):
+        response_json=json.loads(response_string)
+        #assegnamo i campi che risulteranno stringhe
+        enc_session_key=response_json["enc_session_key"]
+        tag =response_json["tag"]
+        ciphertext=response_json["ciphertext"]
+        nonce=response_json["nonce"]
+        private_key = RSA.import_key(open(path_private_key).read())
+        enc_session_key_bytes=b64decode(enc_session_key)
+        tag_bytes = b64decode(tag)
+        ct_bytes = b64decode(ciphertext)
+        nonce_bytes=b64decode(nonce.encode())
+        # Decrypt the session key with the private RSA key
+        cipher_rsa = PKCS1_OAEP.new(private_key)
+        session_key = cipher_rsa.decrypt(enc_session_key_bytes)
+        # Decrypt the data with the AES session key
+        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce_bytes)
+        secret_byte = cipher_aes.decrypt_and_verify(ct_bytes, tag_bytes)
+        return secret_byte
+        
+    def public_server_key_encrypt(self,secret_byte, path_public_key):
+        psk=RSA.import_key(open(path_public_key).read())
+        session_key = get_random_bytes(16)
+        cipher_rsa = PKCS1_OAEP.new(psk)
+        enc_session_key_bytes = cipher_rsa.encrypt(session_key)
+         # Encrypt the data with the AES session key
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        ciphertext_bytes, tag_bytes = cipher_aes.encrypt_and_digest(secret_byte)
+        tag = b64encode(tag_bytes).decode('utf-8')
+        ct = b64encode(ciphertext_bytes).decode('utf-8')
+        enc_session_key = b64encode(enc_session_key_bytes).decode('utf-8')
+        payload_string= json.dumps({'enc_session_key':enc_session_key, 'tag':tag, 'ciphertext':ct, 'nonce':b64encode(cipher_aes.nonce).decode('utf-8')})
+        payload=json.dumps(payload_string).encode("utf-8")
+        return payload
+    
+    async def authentication_client(self):
+        endpoint=self.server_uri+"authentication"
+        '''
+        TO DO
+        hello_str già pensato per inviare al server un json da poter ricercare in config.json del server dove
+        verranno aggiunti le chiavi pubbliche dei sensori e attuatori
+        '''
+        hello_str= json.dumps({'type':'attuatori', 'id':'attuatore'+ str(self.id_client)})
+        payload=json.dumps(hello_str).encode("utf-8") #byte
+        response_hello=await self.send_get_request(endpoint,payload=payload)
+        
+        #il client ha ricevuto un messaggio dal server contenente la challenge
+        response_string=json.loads(response_hello.payload.decode())
+        secret_byte=self.private_client_key_decrypt(response_string,"./src/attuatore/private_attuatore.pem")
 
+        #ora si cifra con la chiave pubblica del server e si manda la risposta
+        payload=self.public_server_key_encrypt(secret_byte,"./src/sensore/public_server.pem")
+       
+        response_challenge=await self.send_post_request(endpoint,payload=payload)
+        #il client ha ricevuto le chiavi e se le prende
+        response_string=json.loads(response_challenge.payload.decode())
+        secret_byte=self.private_client_key_decrypt(response_string,"./src/attuatore/private_attuatore.pem")
+        
+        secret_json=json.loads(secret_byte.decode())
+        self.aes_key= b64decode(secret_json["aes"])
+        self.hmac_key=b64decode(secret_json["hmac"])
+        print("Autenticazione riuscita, chiavi correttamente memorizzate")
+        
     async def state_request(self):
         '''
         Invia una richiesta al server per conoscere in quale stato deve essere l'attuatore
@@ -132,6 +199,31 @@ class AttuatoreSecure(Attuatore):
         else:
             logging.info(Fore.GREEN+f"Errore nella risposta del server: {response.code}")
             return None
+    async def send_post_request(self, endpoint,payload):
+        '''
+        Metodo che invia ad un endpoint una post e restituisce la risposta alla richiesta, restiutisce None in caso di insuccesso
+        '''
+        try:
+            protocol = await aiocoap.Context.create_client_context()
+            request = Message(code=aiocoap.POST, uri=endpoint, payload=payload)
+            logging.info(Fore.GREEN+"Richiesta inviata")
+
+            response = await protocol.request(request).response
+            print(response)
+        except aiocoap.error.RequestTimedOut:
+            logging.info(Fore.GREEN+"Richiesta al server CoAP scaduta")
+            return None
+        if response.code.is_successful():
+            try:
+                logging.info(Fore.GREEN+"Il server ha inviato una risposta valida")
+                return response
+            except ValueError:
+                logging.info(Fore.GREEN+"Il server ha inviato una risposta non valida")
+                return None
+        else:
+            logging.info(Fore.GREEN+f"Errore nella risposta del server: {response.code}")
+            return None 
+    
 
 
 def main():
@@ -143,6 +235,7 @@ def main():
         if  attuatore.mode=="loop":
             iter=0
             loop=asyncio.get_event_loop()
+            loop.run_until_complete(attuatore.authentication_client())
             while True:
                 time.sleep(attuatore.time_unit)
                 #Inserire qui i metodi di routine
