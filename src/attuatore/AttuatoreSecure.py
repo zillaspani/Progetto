f9e7f74869639695a0241fd2821d775517212047
Sodus4 <sodusss12@gmail.com>
2023-11-29 16:58:00 +0100

    Mandarino dove sei

--- src/attuatore/AttuatoreSecure.py
+++ src/attuatore/AttuatoreSecure.py
@@ -12,7 +12,7 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
-
+#
 
 class AttuatoreSecure(Attuatore):
     '''chiavi inizialmente messe qui per prova, 16 byte per AES, 32 per HMAC, da spostare il un file di configurazione poi'''
