664254d3e464a31baabfca00d4fab2f1749983a8
zillaspani <garofalo.francesco@outlook.it>
2023-11-30 16:10:20 +0100

    Provala

--- logger.py
+++ logger.py
@@ -29,7 +29,7 @@ def analyze_ram_and_cpu_of_a_process(process_name, maximum = number_of_cpu):
 
         
     with open(process_name+".csv", "x") as CSV:
-        CSV.write("TIME,CPU,RAM,CPU_W,IF_WLAN_W,TOT_W,MWH \n")
+        CSV.write("TIME,CPU,RAM,CPU_W,WIFI_UP_W,WIFI_DOWN_W,TOT_W,MWH \n")
         process_pid = get_process_pid(process_name)
         process = psutil.Process(process_pid)
         print("Start logging on "+process_name)
@@ -51,17 +51,19 @@ def analyze_ram_and_cpu_of_a_process(process_name, maximum = number_of_cpu):
                 bar.update(i)
                 packets_counter_old=psutil.net_io_counters(pernic=True)[interface]
                 process_cpu = process.cpu_percent(interval)
-                
                 process_ram = process.memory_percent()
                 packets_counter=psutil.net_io_counters(pernic=True)[interface]
                 delta=time.time()-time_start
                 process_cpu_energy=1.5778+0.181*process_cpu
-                packets_total=packets_counter[0]+packets_counter[1]-packets_counter_old[0]-packets_counter_old[1]
-                packet_rate=packets_total/delta*pow(10,-6)
-                process_network_energy=0.942+0.064+4.813*pow(10,-3)*packet_rate
-                total_w=process_cpu_energy+process_network_energy
+                packets_total_up=packets_counter[0]-packets_counter_old[0]
+                packets_total_down=packets_counter[1]-packets_counter_old[1]
+                packet_rate_up=packets_total_up/delta*pow(10,-6)
+                packet_rate_down=packets_total_down/delta*pow(10,-6)
+                process_network_energy_up=0.064+4.813*pow(10,-3)*packet_rate_up
+                process_network_energy_down=0.057+4.813*pow(10,-3)*packet_rate_down
+                total_w=process_cpu_energy+process_network_energy_up+process_network_energy_down+0.942
                 total_mwh+=total_w*delta*mwh_cost
-                CSV.write(str(round(time.time()-time_s,2))+","+str(process_cpu)+","+str(round(process_ram,2))+","+str(round(process_cpu_energy,2))+","+str(round(process_network_energy,2))+","+str(round(total_w,2))+","+str(round(total_mwh,2))+"\n")
+                CSV.write(str(round(time.time()-time_s,2))+","+str(process_cpu)+","+str(round(process_ram,2))+","+str(round(process_cpu_energy,2))+","+str(round(process_network_energy_up,2))+","+str(round(process_network_energy_down,2))+","+str(round(total_w,2))+","+str(round(total_mwh,2))+"\n")
                 i+=1    
             # Termina tcpdump
             tcpdump_process.terminate()
